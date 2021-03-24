d035c4007d3eb3e2a0c6d882b281f9f9179a0915
zodiacVG <zhangzibo418@outlook.com>
2021-03-24 17:05:46 +0800

    完成toast

--- lab2/app/src/main/java/com/example/helloworld/MainActivity.java
+++ lab2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.helloworld;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "HelloWorld");
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
     }
 }
