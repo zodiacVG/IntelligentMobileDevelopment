089113e2a27b7b51fec0339daa8b9de270037e2b
zodiacVG <zhangzibo418@outlook.com>
2021-03-25 11:04:00 +0800

    完成竖屏所有功能

--- lab2/app/src/main/java/com/example/helloworld/MainActivity.java
+++ lab2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,8 +26,26 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    @SuppressLint("ResourceAsColor")
     public void countUp(View view) {
         mCount++;
+        if (mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount)); //textview的settext方法
+            if(mCount % 2==0)
+                mShowCount.setTextColor(R.color.colorButtonPressed);
+            else
+//                mShowCount.setTextColor(R.color.colorPrimary);
+                    mShowCount.setTextColor(this.getResources().getColor(R.color.colorPrimary));
+        }
+
+    }
+
+    public void zeroCount(View view) {
+        if(mCount==0){
+            Toast toast2=Toast.makeText(this,"已经归零了！",Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+        mCount=0;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount)); //textview的settext方法
     }
