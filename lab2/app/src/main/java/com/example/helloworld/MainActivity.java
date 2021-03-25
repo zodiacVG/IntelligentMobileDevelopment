561d81ab2d72f16beb90cec14743c4a3011c7636
zodiacVG <zhangzibo418@outlook.com>
2021-03-25 09:18:05 +0800

    完成linearLayout

--- lab2/app/src/main/java/com/example/helloworld/MainActivity.java
+++ lab2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,17 +3,21 @@ package com.example.helloworld;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCount=0;
+    private TextView mShowCount; //把xml里面的textview抓过来
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "HelloWorld");
+        mShowCount=(TextView)findViewById(R.id.show_count); //拿到了xml文件里面的view，这一步需要在oncreate里面完成
     }
 
     public void showToast(View view) {
@@ -22,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount)); //textview的settext方法
     }
 }
