bc6efc729ce647195aad94f3da4311143aac396f
zodiacVG <zhangzibo418@outlook.com>
2021-03-25 14:00:26 +0800

    完成全部功能

--- lab2/app/src/main/java/com/example/helloworld/MainActivity.java
+++ lab2/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount=0;
     private TextView mShowCount; //把xml里面的textview抓过来
+    private Button mZeroButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "HelloWorld");
         mShowCount=(TextView)findViewById(R.id.show_count); //拿到了xml文件里面的view，这一步需要在oncreate里面完成
+        mZeroButton=(Button)findViewById(R.id.button_zero);
+        mZeroButton.setBackgroundColor(this.getResources().getColor(R.color.colorButtonPressed));
     }
 
     public void showToast(View view) {
@@ -31,16 +35,18 @@ public class MainActivity extends AppCompatActivity {
         mCount++;
         if (mShowCount != null){
             mShowCount.setText(Integer.toString(mCount)); //textview的settext方法
-            if(mCount % 2==0)
+            if(mCount % 2==0)  //奇偶变换颜色
                 mShowCount.setTextColor(R.color.colorButtonPressed);
             else
-//                mShowCount.setTextColor(R.color.colorPrimary);
-                    mShowCount.setTextColor(this.getResources().getColor(R.color.colorPrimary));
+                mShowCount.setTextColor(this.getResources().getColor(R.color.colorPrimary));
+            if(mCount==1)
+                mZeroButton.setBackgroundColor(this.getResources().getColor(R.color.colorNormal));
         }
 
     }
 
     public void zeroCount(View view) {
+        mZeroButton.setBackgroundColor(this.getResources().getColor(R.color.colorButtonPressed));
         if(mCount==0){
             Toast toast2=Toast.makeText(this,"已经归零了！",Toast.LENGTH_SHORT);
             toast2.show();
