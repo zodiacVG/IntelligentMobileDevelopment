06f8978fc753b49d88dca4aca7a44859bcbb2193
zodiacVG <zhangzibo418@outlook.com>
2021-03-31 17:26:50 +0800

    完成codelab02.1

--- lab4/app/src/main/java/com/example/twactivities/MainActivity.java
+++ lab4/app/src/main/java/com/example/twactivities/MainActivity.java
@@ -6,20 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
     private static final String LOG_TAG =MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.android.twactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     //启动新窗口界面
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent=new Intent(this,SecondActivity.class);
-        startActivity(intent);
+        String message = mMessageEditText.getText().toString(); //获取文本框内容
+        intent.putExtra(EXTRA_MESSAGE,message); //加信息
+//        startActivity(intent);   如果想要收到回复消息，不能使用这个方法，而要用下面的
+        startActivityForResult(intent,TEXT_REQUEST); //使用这个方法想收到消息需要指定一个request代码
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- lab4/app/src/main/java/com/example/twactivities/SecondActivity.java
+++ lab4/app/src/main/java/com/example/twactivities/SecondActivity.java
@@ -2,13 +2,32 @@ package com.example.twactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    public static final String EXTRA_REPLY = "com.example.android.twactivities.extra.REPLY";
+    private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second); //获取回复框信息
+        //获取信息
+        Intent intent=getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent(); //创建一个新的intent用于返回reply
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        setResult(RESULT_OK,replyIntent); //附加信息，收到了之前的
+        finish(); //todo 并没有发送的过程，为什么？
     }
 }
