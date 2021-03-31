6d86f4560ffdd290538803962eb934cf91a8eb42
zodiacVG <zhangzibo418@outlook.com>
2021-03-31 16:43:06 +0800

    实现界面跳转

--- lab4/app/src/main/java/com/example/twactivities/MainActivity.java
+++ lab4/app/src/main/java/com/example/twactivities/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.twactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG =MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //启动新窗口界面
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
