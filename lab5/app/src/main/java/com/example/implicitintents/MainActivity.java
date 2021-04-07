3ea37d73ace96a5ebb7d83a0ebaebbd31696f7a8
zodiacVG <zhangzibo418@outlook.com>
2021-04-07 16:49:56 +0800

    做完了发送端，但是并没有成功

---
+++ lab5/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url); //url转化为uri
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage); //implicit intent的两个参数是这样传递的！
+        if (intent.resolveActivity(getPackageManager()) != null) {  //寻找可以执行这个指令的app
+            startActivity(intent);
+        }else{
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc); //geo??
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void shareText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain"; //todo 定义分享mime（what？）
+        ShareCompat.IntentBuilder //
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.shareChoosterTitle)
+                .setText(txt)
+                .startChooser();
+    }
+}
